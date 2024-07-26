8f90e0bdb580f46b4c4b0e1d4bb6409fbc3ebb9e
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-07-25 17:23:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,87 +1,65 @@
-addEventListener("keydown", function (event) {
+// Add event listener for 'Enter' key press
+document.addEventListener("keydown", function(event) {
   if (event.key === "Enter") search();
 });
 
-function displaySE() {
+// Retrieve the selected search engine
+function getSelectedSearchEngine() {
   var currentSE = document.getElementById("SE");
-  var se = currentSE[currentSE.selectedIndex].id;
-  alert("You are now searching on " + se);
-  var SE = currentSE[currentSE.selectedIndex].value;
-  return SE;
+  return currentSE[currentSE.selectedIndex].value;
 }
 
-function fakeset(box, boxid) {
-  boxid = box[box.selectedIndex].id;
-  alert("you are now searching on " + boxid);
+// Alert the selected search engine
+function displaySearchEngine(box) {
+  alert(`You are now searching on ${box[box.selectedIndex].id}`);
 }
 
-function settings(div) {
-  if (div.style.display === "none") {
-    div.style.display = "block";
-  } else if (div.style.display === "block") {
-    div.style.display = "none";
-  }
+// Toggle display of a div
+function toggleVisibility(div) {
+  div.style.display = (div.style.display === "none") ? "block" : "none";
 }
 
+// Perform search
 function search() {
   var searchbox = document.getElementById("input");
-  var url = searchbox.value;
-  if (url.includes("://")) {
-    window.location = url;
-    searchbox.value = "";
-  } else if (url.includes(".")) {
-    window.location = "https://" + url;
-    searchbox.value = "";
-  } else {
-    var engineval = document.getElementById("SE");
-    var engine = engineval[engineval.selectedIndex].value;
-    window.location = engine + url;
-    searchbox.value = "";
-  }
+  var url = searchbox.value.trim();
+  
+  if (url.includes("://")) window.location = url;
+  else if (url.includes(".")) window.location = "https://" + url;
+  else window.location = getSelectedSearchEngine() + url;
+  
+  searchbox.value = "";
 }
 
-function bookmark(event) {
+// Add a bookmark
+function bookmark() {
   var myTextInput = document.getElementById("bookmarksite");
-  var url = myTextInput.value;
-  if (url.includes("https://")) {
-    var a = document.createElement("a");
-    var br = document.createElement("br");
-    var linkText = document.createTextNode(url);
-    var bmspos = document.getElementById("bmsdiv");
-    a.appendChild(linkText);
+  var url = myTextInput.value.trim();
+  var bmspos = document.getElementById("bmsdiv");
+  var a = document.createElement("a");
+  var br = document.createElement("br");
+  var linkText;
+  
+  if (url.startsWith("https://")) {
+    linkText = document.createTextNode(url);
     a.title = url;
     a.href = url;
-    a.target = "blank_";
-    myTextInput.value = "";
-    bmspos.appendChild(a);
-    bmspos.appendChild(br);
   } else if (url.includes(".")) {
-    var a = document.createElement("a");
-    var br = document.createElement("br");
-    var linkText = document.createTextNode("https://" + url);
-    var bmspos = document.getElementById("bmsdiv");
-    a.appendChild(linkText);
+    linkText = document.createTextNode("https://" + url);
     a.title = "https://" + url;
     a.href = "https://" + url;
-    a.target = "blank_";
-    bmspos.appendChild(a);
-    bmspos.appendChild(br);
-    myTextInput.value = "";
   } else {
-    var engineval = document.getElementById("SE");
-    var engine = engineval[engineval.selectedIndex].value;
-    var a = document.createElement("a");
-    var br = document.createElement("br");
-    var linkText = document.createTextNode(url);
-    var bmspos = document.getElementById("bmsdiv");
-    a.appendChild(linkText);
+    var engine = getSelectedSearchEngine();
+    linkText = document.createTextNode(url);
     a.id = "nounderline";
     a.title = engine + url;
     a.href = engine + url;
-    a.target = "blank_";
-    bmspos.appendChild(a);
-    bmspos.appendChild(br);
-
-    myTextInput.value = "";
   }
+  
+  a.appendChild(linkText);
+  a.target = "blank_";
+  bmspos.appendChild(a);
+  bmspos.appendChild(br);
+  
+  myTextInput.value = "";
 }
